630338750eecf3d08e4b4d84596f89ecf9720218
Ken Ellis <kaellis@gmail.com>
2021-12-08 17:00:08 -0500

    catch and log non-json responses from node

--- src/pytezos/rpc/node.py
+++ src/pytezos/rpc/node.py
@@ -53,7 +53,11 @@ class RpcError(Exception):
     def from_response(cls, res: requests.Response) -> 'RpcError':
         """Create RpcError from requests Response."""
         if res.headers.get('content-type') == 'application/json':
-            errors = res.json()
+            try:
+                errors = res.json()
+            except JSONDecodeError:
+                # sometimes rpc returns invalid json
+                return RpcError(res.text)
             assert isinstance(errors, list)
             return cls.from_errors(errors)
         else:
